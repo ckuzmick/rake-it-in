531d24bdd1a55109e615da3c62dbeb2c9588faa1
ckuzmick <conrad.kuzmick@icloud.com>
2023-10-13 19:34:57 -0400

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         <h1 className="text-2xl pb-10">Rake It In</h1>
         <p>Need leaves raked?</p>
         <p>Need plants watered?</p>
+        <p>Need lawn mowing?</p>
         <p>Need pets to be taken care of?</p>
         <p>Have any odd jobs that need to be done?</p>
         <br/>
