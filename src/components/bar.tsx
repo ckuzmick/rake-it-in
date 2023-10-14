93263a99f71fcc80d24129e03cdc32476b6ecb8e
ckuzmick <33187769+ckuzmick@users.noreply.github.com>
2023-10-14 14:58:06 -0400

    cgabtes

--- src/components/bar.tsx
+++ src/components/bar.tsx
@@ -5,7 +5,7 @@ export default function Bar() {
       <div className="bg-white text-slate-900 p-3 border-solid border-slate-400 border-b-2 xs:text-left sm:text-center">
         <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/pricing">Pricing</a>
         <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/availability">Availability</a>
-        <a className="sm:p-3 xs:p-0 text-2xl" href="/">Rake It In</a>
+        <a className="sm:p-3 xs:p-0 text-3xl" href="/">Rake It In</a>
         <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/services">Services</a>
         <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/about-us">About Us</a>
       </div>
