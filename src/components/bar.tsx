4093a2183a7727fd7c11a423290cd9aba4008ba6
ckuzmick <conrad.kuzmick@icloud.com>
2023-10-15 08:39:09 -0400

    stuff

--- src/components/bar.tsx
+++ src/components/bar.tsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Bar() {
     return (
       <div className="bg-white text-slate-900 p-3 border-solid border-slate-400 border-b-2 xs:text-left sm:text-center">
-        <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/pricing">Pricing</a>
-        <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/availability">Availability</a>
+        {/* <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/pricing">Pricing</a>
+        <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/availability">Availability</a> */}
         <a className="sm:p-3 xs:p-0 text-3xl" href="/">Rake It In</a>
-        <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/services">Services</a>
-        <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/about-us">About Us</a>
+        {/* <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/services">Services</a>
+        <a className="p-3 hover:underline underline-offset-4 xs:hidden sm:inline" href="/about-us">About Us</a> */}
       </div>
     )
   }
